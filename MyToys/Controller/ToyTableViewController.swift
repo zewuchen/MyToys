3a05abcac0373949dde23728e801ad1f072884eb
Zewu Chen <zewu.chen@etec.sp.gov.br>
2020-01-20 22:31:46 -0300

    adicionado a documentação do código

--- MyToys/Controller/ToyTableViewController.swift
+++ MyToys/Controller/ToyTableViewController.swift
@@ -12,20 +12,20 @@ class ToyTableViewController: UITableViewController, UITextFieldDelegate, UIText
 
     let limiteNome = 30
     let limiteQuantidade = 5
-    
+
     @IBOutlet weak var txtNome: UITextField!
     @IBOutlet weak var txtQuantidade: UITextField!
     @IBOutlet weak var lblTamanho: UILabel!
     @IBOutlet weak var lblFaixaEtaria: UILabel!
     @IBOutlet weak var txtViewObservacoes: UITextView!
-    
+
     @IBOutlet weak var navigationBar: UINavigationItem!
     @IBOutlet weak var btnProximo: UIBarButtonItem!
-    
+
     var tamanho:String = ""
     var faixaEtaria:String = ""
     var edit:Bool = false
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.txtNome.delegate = self
@@ -34,12 +34,12 @@ class ToyTableViewController: UITableViewController, UITextFieldDelegate, UIText
         self.txtQuantidade.addTarget(self, action: #selector(txtQuantidadeDidChange(_:)), for: .editingChanged)
         self.txtViewObservacoes.delegate = self
         btnProximo.isEnabled = false
-        
+
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tap.cancelsTouchesInView = false
         self.view.addGestureRecognizer(tap)
-        
-        
+
+
         //Editando brinquedo
         if edit == true{
             txtNome.text = Toy.shared.nome
@@ -50,34 +50,34 @@ class ToyTableViewController: UITableViewController, UITextFieldDelegate, UIText
                 txtViewObservacoes.text = Toy.shared.observacoes
             }
             self.navigationBar.title = "Editar Brinquedo"
-            
+
             Toy.shared.edit = true
         }else{
             Toy.shared.clear()
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         lblTamanho.text = Toy.shared.tamanho
         lblFaixaEtaria.text = Toy.shared.faixaEtaria
-        
+
         if txtViewObservacoes.text == ""{
             txtViewObservacoes.text = "Observações"
             txtViewObservacoes.textColor = .lightGray
         }
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if (indexPath == [1, 0] || indexPath == [1, 1]), edit == true{
             btnProximo.isEnabled = true
         }
     }
-    
-    
+
+
     @objc func dismissKeyboard() {
         self.view.endEditing(true)
     }
-    
+
     //Limite de caracteres
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         if textField == self.txtNome{
@@ -85,15 +85,15 @@ class ToyTableViewController: UITableViewController, UITextFieldDelegate, UIText
         }
         return txtQuantidade.text!.count + string.count <= self.limiteQuantidade
     }
-    
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == txtNome{
             self.view.endEditing(true)
         }
-        
+
         return true
     }
-    
+
     //Placeholder no TextView
     func textViewDidBeginEditing(_ textView: UITextView)
     {
@@ -104,7 +104,7 @@ class ToyTableViewController: UITableViewController, UITextFieldDelegate, UIText
         }
         textView.becomeFirstResponder()
     }
-    
+
     func textViewDidEndEditing(_ textView: UITextView)
     {
         if (textView.text == "")
@@ -115,26 +115,31 @@ class ToyTableViewController: UITableViewController, UITextFieldDelegate, UIText
         checkObservacoes()
         textView.resignFirstResponder()
     }
-    
+
     @IBAction func btnProximoAction(_ sender: Any) {
         Toy.shared.nome = txtNome.text
         Toy.shared.quantidade = Int64(txtQuantidade.text!)
         Toy.shared.observacoes = txtViewObservacoes.text
-        
+
         let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: "camera")
         self.navigationController!.pushViewController(controller, animated: true)
     }
-    
+
     @IBAction func txtNomeDidChange(_ sender: Any) {
         checkInputValues()
-        
+
     }
-    
+
     @IBAction func txtQuantidadeDidChange(_ sender: Any) {
         checkInputValues()
     }
-    
+
+    /**
+    *Checa se os campos (nome, quantidade) estão preenchidos*
+    - Parameters: Nenhum
+    - Returns: Nenhum
+    */
     func checkInputValues() {
         if txtNome.text != nil, txtNome.text != "", txtQuantidade.text != nil, txtQuantidade.text != "0", txtQuantidade.text != ""{
             btnProximo.isEnabled = true
@@ -142,6 +147,12 @@ class ToyTableViewController: UITableViewController, UITextFieldDelegate, UIText
             btnProximo.isEnabled = false
         }
     }
+
+    /**
+    *Checa se os campos (nome, quantidade, observações) estão preenchidos*
+    - Parameters: Nenhum
+    - Returns: Nenhum
+    */
     func checkObservacoes() {
         if txtViewObservacoes.text != Toy.shared.observacoes, txtNome.text != nil, txtNome.text != "", txtQuantidade.text != nil, txtQuantidade.text != "0", txtQuantidade.text != ""{
             btnProximo.isEnabled = true
@@ -149,5 +160,5 @@ class ToyTableViewController: UITableViewController, UITextFieldDelegate, UIText
             btnProximo.isEnabled = false
         }
     }
-    
+
 }
